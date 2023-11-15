4091f8c296ec5022bfc9d8e5dade3e89809ff97e
MinhPhu <minhphuzcod@gmail.com>
2023-11-15 19:12:44 +0700

    Export file to txt

--- Network_Progaming/src/week6/StudentManager.java
+++ Network_Progaming/src/week6/StudentManager.java
@@ -2,8 +2,11 @@ package week6;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -13,7 +16,8 @@ public class StudentManager {
 	public static List<Student> loadData(String stFile, String gradeFile, String charSet)
 			throws NumberFormatException, IOException {
 		List<Student> list = new ArrayList<>();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(stFile)));
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(stFile), charSet));
 		String line, token;
 		StringTokenizer st;
 		int id;
@@ -35,28 +39,42 @@ public class StudentManager {
 			st = new StringTokenizer(line, "\t");
 			id = Integer.parseInt(st.nextToken());
 			int count = 0;
-			double value = 0;
+			double values = 0;
 			while (st.hasMoreTokens()) {
 				count++;
-				value += Double.parseDouble(st.nextToken());
+				values += Double.parseDouble(st.nextToken());
 			}
+			grade = values / count;
 		}
+
 		reader.close();
 
 		return list;
 	}
 
-	public static void export(List<Student> list, String textFile, String charset) {
+	public static void export(List<Student> list, String textFile, String charset)
+			throws FileNotFoundException, UnsupportedEncodingException {
+		PrintWriter pw = new PrintWriter(textFile, charset);
+		for (Student st : list) {
+			String line = st.id + "\t" + st.name + "\t" + st.year + "\t" + st.grade;
+			pw.println(line);
+		}
+		pw.close();
 
 	}
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		String stFile = "D:\\Trash\\Test\\sinhvien.txt";
-		String gradeFile = "D:\\Trash\\Test\\diem.txt";
+		String stFile = "D:\\Project\\Network-Programing\\Network_Progaming\\Test\\Tuan6\\sinhvien.txt";
+		String gradeFile = "D:\\Project\\Network-Programing\\Network_Progaming\\Test\\Tuan6\\diem.txt";
 		String charSet = "UTF-8";
 
 		List<Student> list = loadData(stFile, gradeFile, charSet);
-		System.out.println(list);
+
+//		System.out.println(list);
+
+		String expFile = "D:\\Project\\Network-Programing\\Network_Progaming\\Test\\Tuan6\\export.txt";
+		String expCharSet = "UTF-16";
+		export(list, expFile, expCharSet);
 	}
 
 }
